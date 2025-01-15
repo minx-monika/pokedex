1719bd1e39dbe5e56ef82284579a3e3fd6c491b4
Monika Gupta <115791268+minx-monika@users.noreply.github.com>
2025-01-15 17:28:14 +0530

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -4,9 +4,11 @@ const searchInput = document.querySelector("#search-input");
 const numberFilter = document.querySelector("#number");
 const nameFilter = document.querySelector("#name");
 const notFoundMessage = document.querySelector("#not-found-message");
+const closeButton = document.querySelector(".search-close-icon");
 
 let allPokemons = [];
 
+// Fetch Pokemon list
 fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
   .then((response) => response.json())
   .then((data) => {
@@ -14,6 +16,7 @@ fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
     displayPokemons(allPokemons);
   });
 
+// Fetch individual Pokemon data before redirecting
 async function fetchPokemonDataBeforeRedirect(id) {
   try {
     const [pokemon, pokemonSpecies] = await Promise.all([
@@ -30,6 +33,7 @@ async function fetchPokemonDataBeforeRedirect(id) {
   }
 }
 
+// Display Pokemons
 function displayPokemons(pokemon) {
   listWrapper.innerHTML = "";
 
@@ -45,7 +49,7 @@ function displayPokemons(pokemon) {
             <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}" />
         </div>
         <div class="name-wrap">
-            <p class="body3-fonts">#${pokemon.name}</p>
+            <p class="body3-fonts">${pokemon.name}</p>
         </div>
     `;
 
@@ -60,8 +64,7 @@ function displayPokemons(pokemon) {
   });
 }
 
-searchInput.addEventListener("keyup", handleSearch);
-
+// Handle search functionality
 function handleSearch() {
   const searchTerm = searchInput.value.toLowerCase();
   let filteredPokemons;
@@ -88,11 +91,29 @@ function handleSearch() {
   }
 }
 
-const closeButton = document.querySelector(".search-close-icon");
-closeButton.addEventListener("click", clearSearch);
-
+// Clear search input and reset display
 function clearSearch() {
   searchInput.value = "";
   displayPokemons(allPokemons);
   notFoundMessage.style.display = "none";
 }
+
+// Add event listeners
+searchInput.addEventListener("keyup", handleSearch);
+closeButton.addEventListener("click", clearSearch);
+
+// Toggle collapsible menu
+const menuToggle = document.querySelector(".menu-toggle");
+const leftMenu = document.querySelector(".left-menu");
+
+menuToggle.addEventListener("click", () => {
+  leftMenu.classList.toggle("active");
+});
+
+// Handle sort functionality
+const sortFilters = document.querySelectorAll("input[name='filters']");
+sortFilters.forEach((filter) =>
+  filter.addEventListener("change", () => {
+    handleSearch(); // Reapply filtering whenever sorting changes
+  })
+);
